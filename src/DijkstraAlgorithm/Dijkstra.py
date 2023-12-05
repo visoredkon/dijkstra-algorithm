a5e82fc166115732bf7d86145ebfcf24394d1fbd
Pahril <88573655+visoredkon@users.noreply.github.com>
2023-12-06 04:07:06 +0800

    Add previous vertex tracking in Dijkstra algorithm

--- src/DijkstraAlgorithm/Dijkstra.py
+++ src/DijkstraAlgorithm/Dijkstra.py
@@ -4,13 +4,27 @@ from tabulate import tabulate
 class Dijkstra:
     def __init__(self, graph):
         self.graph = graph
-
         self.distances = {vertex: float("infinity") for vertex in self.graph.vertices}
         self.paths = {vertex: [] for vertex in self.graph.vertices}
         self.visited = set()
+        self.previous = {vertex: None for vertex in self.graph.vertices}
 
     def find_path(self, start, finish):
         self.distances[start] = 0
+        self.previous[start] = start
+
+        superscript_map = {
+            "0": "⁰",
+            "1": "¹",
+            "2": "²",
+            "3": "³",
+            "4": "⁴",
+            "5": "⁵",
+            "6": "⁶",
+            "7": "⁷",
+            "8": "⁸",
+            "9": "⁹",
+        }
 
         table_header = [f"   {vertex}   " for vertex in self.graph.vertices]
         table_header.insert(0, "Visited")
@@ -33,10 +47,19 @@ class Dijkstra:
                     self.paths[vertex_to] = self.paths[current_vertex] + [
                         current_vertex
                     ]
+                    self.previous[vertex_to] = current_vertex
 
             self.visited.add(current_vertex)
             table_data.append([current_vertex])
-            [table_data[-1].append(f"{weight}") for _, weight in self.distances.items()]
+            [
+                table_data[-1].append(
+                    "".join(superscript_map[i] for i in str(weight))
+                    + self.previous[vertex]
+                    if weight != float("infinity")
+                    else "∞"
+                )
+                for vertex, weight in self.distances.items()
+            ]
 
         table = tabulate(
             table_data,
